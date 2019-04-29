8a00d8e3cfab3d9cae2b8999a038a72abc6abe79
nocturnal <foxnezz@gmail.com>
2019-04-29 15:44:31 +0300

    added base crop

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import matplotlib.image as mpimg
 
 CROP_Y_TOP = 357
 CROP_Y_BOTTOM = 1765
+PLOT_X = 45
+WHITE = 1
+BLACK = 0
 
 def get_img():
     return mpimg.imread('asd.png')
@@ -11,8 +14,8 @@ def get_img():
 def darken(image):
     img = image.copy()
     mask = img < 0.999
-    img[mask] = 0
-    img[np.logical_not(mask)] = 1
+    img[mask] = BLACK
+    img[np.logical_not(mask)] = WHITE
     return img
 
 def crop(image):
@@ -25,17 +28,23 @@ def show(image):
 def rgb2gray(image): #[0.2989, 0.5870, 0.1140] [0.1140, 0.5870, 0.2989]
     return np.dot(image[...,:3], [0.2989, 0.5870, 0.1140])
 
-def mark_plot(image):
+def base_crop(image):
     for y in range(image.shape[0]):
-        if image[y, 44] == 1 and image[y, 45] == 0:
-            print('({}, {})'.format(45, y))
+        if image[y, PLOT_X - 1] == WHITE and image[y, PLOT_X] == BLACK:
+            base = y
+            break
+    
+    while image[base + 1, PLOT_X] == BLACK:
+        base += 1
+    
+    return image[:base, :]
 
 def main():
     image = get_img()
     image = crop(image)
     image = rgb2gray(image)
     image = darken(image)
-    mark_plot(image)
+    image = base_crop(image)
     show(image)
 
 main()

a570bd129ca697a04e329992395b60e26f86f283
nocturnal <foxnezz@gmail.com>
2019-04-29 15:07:25 +0300

    added grayscaling

--- main.py
+++ main.py
@@ -10,21 +10,32 @@ def get_img():
 
 def darken(image):
     img = image.copy()
-    mask = img != 1
+    mask = img < 0.999
     img[mask] = 0
+    img[np.logical_not(mask)] = 1
     return img
 
 def crop(image):
     return image[CROP_Y_TOP:CROP_Y_BOTTOM, :]
 
 def show(image):
-    plt.imshow(image)
+    plt.imshow(image, cmap = 'gray')
     plt.show()
+
+def rgb2gray(image): #[0.2989, 0.5870, 0.1140] [0.1140, 0.5870, 0.2989]
+    return np.dot(image[...,:3], [0.2989, 0.5870, 0.1140])
+
+def mark_plot(image):
+    for y in range(image.shape[0]):
+        if image[y, 44] == 1 and image[y, 45] == 0:
+            print('({}, {})'.format(45, y))
 
 def main():
     image = get_img()
-    image = darken(image)
     image = crop(image)
+    image = rgb2gray(image)
+    image = darken(image)
+    mark_plot(image)
     show(image)
 
 main()
